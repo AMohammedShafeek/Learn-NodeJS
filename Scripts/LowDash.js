33ac025b4dc1d163112d8350b0ccf57221a8f750
AMohammedShafeek <amdshafeek7@gmail.com>
2025-12-15 01:51:08 +0530

    Nodemon & LoDash - Nodemon for Update/refresh Server Fast, Install LoDash and print random Number

---
+++ Scripts/LowDash.js
@@ -0,0 +1,21 @@
+// Importing the built-in HTTP module
+const http = require('http')
+
+//Importing LoDash
+const _ = require('lodash')
+
+// Creating an HTTP server
+// The callback function runs every time a request is received
+const server = http.createServer((req, res) => {
+    console.log("Request Made"); // Logs whenever a client sends a request
+
+    // 'req' contains request details (URL, method, headers)
+    // 'res' is used to send response back to the client
+})
+
+// Starting the server and listening on port 3000
+server.listen(3000, 'localhost', () => {
+    console.log('Server is Listening'); // Confirms server is running
+    console.log(_.random(15,20));
+    
+}) 
