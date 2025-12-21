953d4f52c2f6ee57e15e41969f15411b15b593a2
AMohammedShafeek <amdshafeek7@gmail.com>
2025-12-21 13:14:59 +0530

    ExpressApp: return role based objects with cookies using cookie-parser

--- src/index.mjs
+++ src/index.mjs
@@ -1,16 +1,20 @@
 import express from 'express'
 import router from './routes/router.mjs'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
 //covert data to json formal
 app.use(express.json())
 
+app.use(cookieParser())
+
 app.use(router)
 
 const PORT = 3000
 
 app.get('/', (req, res) => {
+    res.cookie("user", "admin", {maxAge: 60000 * 60})
     res.send({ msg: "Root" })
 })
 
--- src/routes/users.mjs
+++ src/routes/users.mjs
@@ -7,17 +7,24 @@ import { validationResult, matchedData, checkSchema } from 'express-validator'
 const router = Router()
 
 router.get('/api/users', (req, res) => {
-    const {query:{filter, value}} = req
-    if(filter && value){
-        return res.send(users.filter(((user)=>user[filter].toLowerCase().includes(value.toLowerCase()))))
+
+    console.log(req.cookies);
+    if (req.cookies.user && req.cookies.user === "admin") {
+        const { query: { filter, value } } = req
+        if (filter && value) {
+            return res.send(users.filter(((user) => user[filter].toLowerCase().includes(value.toLowerCase()))))
+        }
+        return res.send(users)
     }
-    res.send(users)
+    else{
+        res.send({msg: "You don't have a Access"})
+    }
 })
 
 // Route Params
 //users endpoint
 router.get('/api/users/:id', getParamsId, (req, res) => {
-    const {id} = req
+    const { id } = req
     const user = users.find((user) =>
         (user.id === id)
     )
@@ -28,38 +35,38 @@ router.get('/api/users/:id', getParamsId, (req, res) => {
 })
 
 // Insert new user using POST request
-router.post('/api/users', checkSchema(createUserValidationSchema), (req, res)=>{
+router.post('/api/users', checkSchema(createUserValidationSchema), (req, res) => {
     const result = validationResult(req)
 
-    if(!result.isEmpty()){
-        return res.status(400).send({error: result.array()})
+    if (!result.isEmpty()) {
+        return res.status(400).send({ error: result.array() })
     }
 
     const body = matchedData(req)
-    const newUser = {id: users[users.length-1].id+1, ...body}
+    const newUser = { id: users[users.length - 1].id + 1, ...body }
     users.push(newUser)
     res.status(201).send(newUser)
 })
 
 //Update User using PUT Request
-router.put('/api/users/:id', getUserIndexByID, (req, res)=>{
-    const {userIndex, body} = req
-    users[userIndex] = {id: id, ...body}
-    return res.status(200).send({msg: "User Upated"})
+router.put('/api/users/:id', getUserIndexByID, (req, res) => {
+    const { userIndex, body } = req
+    users[userIndex] = { id: id, ...body }
+    return res.status(200).send({ msg: "User Upated" })
 })
 
 //Update User using PATCH Request
-router.patch('/api/users/:id', getUserIndexByID, (req, res)=>{
-    const {userIndex, body} = req
-    users[userIndex] = {...users[userIndex], ...body}
-    return res.status(200).send({msg: "User Upated"})
+router.patch('/api/users/:id', getUserIndexByID, (req, res) => {
+    const { userIndex, body } = req
+    users[userIndex] = { ...users[userIndex], ...body }
+    return res.status(200).send({ msg: "User Upated" })
 })
 
 //Delete User using DELETE Request
-router.delete('/api/users/:id', getUserIndexByID, (req, res)=>{
-    const {userIndex} = req
+router.delete('/api/users/:id', getUserIndexByID, (req, res) => {
+    const { userIndex } = req
     users.splice(userIndex, 1)
-    return res.status(200).send({msg: "User Deleted"})
+    return res.status(200).send({ msg: "User Deleted" })
 })
 
 
