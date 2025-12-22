2d3e5ac37fa19edca58e3be9867d7faae76ee328
AMohammedShafeek <amdshafeek7@gmail.com>
2025-12-22 15:03:00 +0530

    ExpressApp: Local Login Authentication using passport-local

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,9 @@ import express from 'express'
 import router from './routes/router.mjs'
 import cookieParser from 'cookie-parser'
 import session from 'express-session'
+import { Strategy as LocalStrategy } from 'passport-local'
+import passport from 'passport'
+import { users } from './utils/dataConsts.mjs'
 
 const app = express()
 
@@ -19,24 +22,66 @@ app.use(session({
     }
 }))
 
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new LocalStrategy(
+    {usernameField: 'user_name', passwordField: 'password'}, 
+    (user_name, password, done) => {
+    const user = users.find((user) => (user.user_name == user_name))
+    if (!user) {
+        return done(null, false, { message: 'Invalid Username' })
+    }
+    if (user.password !== password) {
+        return done(null, false, { message: 'Incorrect Password' })
+    }
+    return done(null, user)
+}))
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+}) 
+
+passport.deserializeUser((id, done) => {
+    const user = users.find((u) => (u.id === id))
+    done(null, user || false)
+})
+
 app.use(router)
 
 const PORT = 3000
 
 app.get('/', (req, res) => {
-    res.cookie("user", "admin", {maxAge: 60000 * 60, signed: true})
+    res.cookie("user", "admin", { maxAge: 60000 * 60, signed: true })
     console.log(req.session.id);
     req.sessionStore.get(req.session.id, (err, sessionData) => {
-        if(err){
+        if (err) {
             console.log(err);
-            
+
         }
-        else{
+        else {
             console.log(sessionData);
-            
+
         }
     })
     res.send({ msg: "Root" })
+})
+
+app.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            return next(err)
+        }
+        if (!user) {
+            return res.status(401).json({ message: info?.message || 'Login Failed' })
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err)
+            }
+            return res.json({msg: 'Login Successful', user})
+        })
+    })(req, res, next)
 })
 
 app.listen(PORT, () => {
--- src/utils/dataConsts.mjs
+++ src/utils/dataConsts.mjs
@@ -2,15 +2,18 @@
 export const users = [
     {
         id: 1,
-        user_name: 'Mohammed Shafeek. A'
+        user_name: 'Mohammed Shafeek. A',
+        password: '1234'
     },
     {
         id: 2,
-        user_name: 'Malavika. G'
+        user_name: 'Malavika. G',
+        password: '1234'
     },
     {
         id: 3,
-        user_name: 'Sivaponnuvel .S'
+        user_name: 'Sivaponnuvel .S',
+        password: '1234'
     },
 ]
 
